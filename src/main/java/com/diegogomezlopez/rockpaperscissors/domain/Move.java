9127c132dea74faaf1efbdf7bee08d38dec99074
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-04-02 01:32:29 +0200

    ADD Move enum

---
+++ src/main/java/com/diegogomezlopez/rockpaperscissors/domain/Move.java
@@ -0,0 +1,29 @@
+package com.diegogomezlopez.rockpaperscissors.domain;
+
+public enum Move {
+
+    ROCK("Rock"), PAPER("Paper"), SCISSORS("Scissors");
+
+    private final String move;
+
+    Move(final String move) {
+        this.move = move;
+    }
+
+    public String getMove() {
+        return this.move;
+    }
+
+    public boolean defeat(final Move opponentMove) {
+        switch (this) {
+            case ROCK:
+                return opponentMove.equals(SCISSORS);
+            case PAPER:
+                return opponentMove.equals(ROCK);
+            case SCISSORS:
+                return opponentMove.equals(PAPER);
+            default:
+                throw new IllegalStateException("Invalid ");
+        }
+    }
+}
