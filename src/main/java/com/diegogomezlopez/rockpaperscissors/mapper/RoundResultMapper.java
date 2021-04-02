f96d049a6c29db16c1c42063b50cf3eb4e2d6d50
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-04-02 16:44:34 +0200

    UPDATE RoundResultMapper class to map properly the RoundResult to dto

--- src/main/java/com/diegogomezlopez/rockpaperscissors/mapper/RoundResultMapper.java
+++ src/main/java/com/diegogomezlopez/rockpaperscissors/mapper/RoundResultMapper.java
@@ -3,6 +3,8 @@ package com.diegogomezlopez.rockpaperscissors.mapper;
 import com.diegogomezlopez.rockpaperscissors.domain.RoundResult;
 import com.diegogomezlopez.rockpaperscissors.dto.RoundResultDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,5 +12,10 @@ public interface RoundResultMapper {
 
     RoundResultMapper INSTANCE = Mappers.getMapper(RoundResultMapper.class);
 
+    @Mappings({
+            @Mapping(source = "player1Move.move", target = "player1Move"),
+            @Mapping(source = "player2Move.move", target = "player2Move"),
+            @Mapping(source = "result.result", target = "result")
+    })
     RoundResultDTO roundResultToRoundResultDTO(final RoundResult roundResult);
 }
