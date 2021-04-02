d236af7de4e7501cac4fa8297646b0d1ef656b46
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-04-02 02:58:39 +0200

    ADD RoundResultService implementation

---
+++ src/main/java/com/diegogomezlopez/rockpaperscissors/services/impl/RoundResultServiceImpl.java
@@ -0,0 +1,31 @@
+package com.diegogomezlopez.rockpaperscissors.services.impl;
+
+import com.diegogomezlopez.rockpaperscissors.domain.Move;
+import com.diegogomezlopez.rockpaperscissors.domain.Result;
+import com.diegogomezlopez.rockpaperscissors.domain.RoundResult;
+import com.diegogomezlopez.rockpaperscissors.services.RoundResultService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoundResultServiceImpl implements RoundResultService {
+
+    @Override
+    public RoundResult getRoundResult(final Move player1Move, final Move player2Move) {
+
+        Result result;
+
+        if (player1Move == player2Move) {
+            result = Result.DRAW;
+        } else if (player1Move.defeat(player2Move)) {
+            result = Result.WIN;
+        } else {
+            result = Result.LOSE;
+        }
+
+        return RoundResult.builder()
+                .player1Move(player1Move)
+                .player2Move(player2Move)
+                .result(result)
+                .build();
+    }
+}
