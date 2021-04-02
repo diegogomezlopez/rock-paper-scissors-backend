fcd1aa0f1fd0acae4c4fcd0e808679255baa24fb
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-04-02 18:28:51 +0200

    UPDATE RoundResultService to extends CrudRepository

--- src/main/java/com/diegogomezlopez/rockpaperscissors/services/RoundResultService.java
+++ src/main/java/com/diegogomezlopez/rockpaperscissors/services/RoundResultService.java
@@ -3,7 +3,7 @@ package com.diegogomezlopez.rockpaperscissors.services;
 import com.diegogomezlopez.rockpaperscissors.domain.Move;
 import com.diegogomezlopez.rockpaperscissors.domain.RoundResult;
 
-public interface RoundResultService {
+public interface RoundResultService extends CrudRepository<RoundResult> {
 
     RoundResult getRoundResult(final Move player1Move, final Move player2Move);
 }
